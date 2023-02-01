ad88e3718cf62172cbc664b6cdad18faa9bedcd6
sugam-pokhrel <psugam75@gmail.com>
2023-02-01 16:56:15 +0545

    created am website that fetches data from nasa api

---
+++ nasa.js
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>NASA</title>
+</head>
+<body>
+    <input type="date">
+    <button>Search</button>
+    <h2></h2>
+    <img src="" alt="">
+    <p></p>
+    <iframe src="" frameborder="0"></iframe>
+
+    
+<script src="./nasa.js"></script>    
+</body>
+</html>
